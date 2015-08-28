f462fdbd37e780ddea7773a743c10001d2dc69b7
Mehdi Abaakouk <sileht@sileht.net>
2015-08-28 22:22:58 +0200

    Adding resource update

--- gnocchiclient/v1/client.py
+++ gnocchiclient/v1/client.py
@@ -13,34 +13,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import json
-
 from keystoneclient import session as keystoneclient_session
 
-
-class Manager(object):
-    def __init__(self, client):
-        self.client = client
-
-
-class ResourceManager(Manager):
-    def list(self, resource_type="generic", details=False, history=False):
-        details = "true" if details else "false"
-        history = "true" if history else "false"
-        url = self.client.url("resource/%s?details=%s&history=%s" % (
-            resource_type, details, history))
-        return self.client.api.get(url).json()
-
-    def get(self, resource_type, resource_id):
-        url = self.client.url("resource/%s/%s" % (
-            resource_type, resource_id))
-        return self.client.api.get(url).json()
-
-    def create(self, resource_type, resource):
-        url = self.client.url("resource/%s" % resource_type)
-        return self.client.api.post(
-            url, headers={'Content-Type': "application/json"},
-            data=json.dumps(resource)).json()
+from gnocchiclient.v1 import resource
 
 
 class Client(object):
@@ -56,7 +31,7 @@ class Client(object):
     def __init__(self, auth, *args, **kwargs):
         """Initialize a new client for the Gnocchi v1 API."""
         self.api = keystoneclient_session.Session(auth, *args, **kwargs)
-        self.resource = ResourceManager(self)
+        self.resource = resource.ResourceManager(self)
         self._endpoint = None
 
     @property
--- gnocchiclient/v1/resource.py
+++ gnocchiclient/v1/resource.py
@@ -13,18 +13,48 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+
 from cliff import lister
 from cliff import show
+from oslo_serialization import jsonutils
+
+from gnocchiclient.v1 import base
 
 
-class ResourceList(lister.Lister):
+class ResourceManager(base.Manager):
+    def list(self, resource_type="generic", details=False, history=False):
+        details = "true" if details else "false"
+        history = "true" if history else "false"
+        url = self.client.url("resource/%s?details=%s&history=%s" % (
+            resource_type, details, history))
+        return self.client.api.get(url).json()
+
+    def get(self, resource_type, resource_id):
+        url = self.client.url("resource/%s/%s" % (
+            resource_type, resource_id))
+        return self.client.api.get(url).json()
+
+    def create(self, resource_type, resource):
+        url = self.client.url("resource/%s" % resource_type)
+        return self.client.api.post(
+            url, headers={'Content-Type': "application/json"},
+            data=jsonutils.dumps(resource)).json()
+
+    def update(self, resource_type, resource_id, resource):
+        url = self.client.url("resource/%s/%s" % (resource_type, resource_id))
+        return self.client.api.patch(
+            url, headers={'Content-Type': "application/json"},
+            data=jsonutils.dumps(resource)).json()
+
+
+class CliResourceList(lister.Lister):
     COLS = ('id', 'type',
             'project_id', 'user_id',
             'started_at', 'ended_at',
             'revision_start', 'revision_end')
 
     def get_parser(self, prog_name):
-        parser = super(ResourceList, self).get_parser(prog_name)
+        parser = super(CliResourceList, self).get_parser(prog_name)
         parser.add_argument("--details", action='store_true',
                             help="Show all attributes of generic resources"),
         parser.add_argument("--history", action='store_true',
@@ -47,9 +77,9 @@ class ResourceList(lister.Lister):
         return tuple([resource[k] for k in cls.COLS])
 
 
-class ResourceShow(show.ShowOne):
+class CliResourceShow(show.ShowOne):
     def get_parser(self, prog_name):
-        parser = super(ResourceShow, self).get_parser(prog_name)
+        parser = super(CliResourceShow, self).get_parser(prog_name)
         parser.add_argument("resource_type",
                             default="generic",
                             nargs='?',
@@ -68,24 +98,46 @@ class ResourceShow(show.ShowOne):
         return self.dict2columns(res)
 
 
-class ResourceCreate(show.ShowOne):
+class CliResourceCreate(show.ShowOne):
     def get_parser(self, prog_name):
-        parser = super(ResourceCreate, self).get_parser(prog_name)
+        parser = super(CliResourceCreate, self).get_parser(prog_name)
         parser.add_argument("resource_type",
                             default="generic",
                             nargs='?',
                             help="Type of resource")
-        parser.add_argument("--field", action='append',
-                            help=("field and its value of a attribute "
+        parser.add_argument("-a", "--attribute", action='append',
+                            help=("name and value of a attribute "
                                   "separated with a ':'"))
         return parser
 
-    def take_action(self, parsed_args):
+    @staticmethod
+    def _resource_from_args(parsed_args):
         resource = {}
-        if parsed_args.field:
-            for field in parsed_args.field:
-                field, __, value = field.partition(":")
-                resource[field] = value
+        if parsed_args.attribute:
+            for attr in parsed_args.attribute:
+                attr, __, value = attr.partition(":")
+                resource[attr] = value
+        return resource
+
+    def take_action(self, parsed_args):
+        resource = self._resource_from_args(parsed_args)
         res = self.app.client.resource.create(
             resource_type=parsed_args.resource_type, resource=resource)
+        return self.dict2columns(res)
+
+
+class CliResourceUpdate(CliResourceCreate):
+    def get_parser(self, prog_name):
+        parser = super(CliResourceUpdate, self).get_parser(prog_name)
+        parser.add_argument("resource_id",
+                            default="generic",
+                            help="resource of the resourece")
+        return parser
+
+    def take_action(self, parsed_args):
+        resource = self._resource_from_args(parsed_args)
+        res = self.app.client.resource.update(
+            resource_type=parsed_args.resource_type,
+            resource_id=parsed_args.resource_id,
+            resource=resource)
         return self.dict2columns(res)
