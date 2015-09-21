eaf9e3244992e7e28eee6ad62c5d84d668c2cd11
Mehdi Abaakouk <sileht@sileht.net>
2015-09-21 10:21:27 +0200

    Fix help typo
    This change fixes some help string typos
    
    Change-Id: I4a013f813d8753ecd178becabaf48f9990e5ff57

--- gnocchiclient/v1/archive_policy_cli.py
+++ gnocchiclient/v1/archive_policy_cli.py
@@ -61,14 +61,14 @@ class CliArchivePolicyCreate(show.ShowOne):
 
     def get_parser(self, prog_name):
         parser = super(CliArchivePolicyCreate, self).get_parser(prog_name)
-        parser.add_argument("name", help=("name of the active policy"))
+        parser.add_argument("name", help=("name of the archive policy"))
         parser.add_argument("-b", "--back-window", dest="back_window",
                             type=int,
-                            help=("back window of the active policy"))
+                            help=("back window of the archive policy"))
         parser.add_argument("-m", "--aggregation-method",
                             action="append",
                             dest="aggregation_methods",
-                            help=("aggregation method of the achive policy"))
+                            help=("aggregation method of the archive policy"))
         parser.add_argument("-d", "--definition", action='append',
                             required=True, type=archive_policy_definition,
                             metavar="<DEFINITION>",
