432db803765e977b14a75574cd8bc33d8e6bc45f
Julien Danjou <julien@danjou.info>
2015-12-18 10:11:23 +0100

    Raise the correct exception on archive policy rule not found
    Change-Id: Ie75390a1c7c9e23d9c41bf827b64cfda430fa784
    Closes-Bug: #1527402

---
+++ gnocchiclient/tests/unit/test_exceptions.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import json
+
+from oslotest import base
+from requests import models
+
+from gnocchiclient import exceptions
+
+
+class ExceptionsTest(base.BaseTestCase):
+    def test_from_response(self):
+        r = models.Response()
+        r.status_code = 404
+        r.headers['Content-Type'] = "application/json"
+        r._content = json.dumps(
+            {"description": "Archive policy rule foobar does not exist"}
+        )
+        exc = exceptions.from_response(r, "http://foobar")
+        self.assertIsInstance(exc, exceptions.ArchivePolicyRuleNotFound)
+
