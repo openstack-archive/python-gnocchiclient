openstack-archive/python-gnocchiclient

commit 36fc0bb74267ec71afbaea1dc350b38e64497496

utils: allow to encode params with list/tuple
Change-Id: Ic71d5467f10d8707f6911c9d52c91fda10ce5300

gnocchiclient/utils.py
@@ -147,9 +147,16 @@ def dict_from_parsed_args(parsed_args, attrs):
 
 
 def dict_to_querystring(objs):
-    return "&".join(["%s=%s" % (k, urllib_parse.quote(six.text_type(v)))
-                     for k, v in objs.items()
-                     if v is not None])
+    strings = []
+    for k, values in objs.items():
+        if values:
+            if not isinstance(values, (list, tuple)):
+                values = [values]
+            strings.append("&".join(
+                ("%s=%s" % (k, v)
+                 for v in map(urllib_parse.quote,
+                              map(six.text_type, values)))))
+    return "&".join(strings)
 
 
 # uuid5 namespace for id transformation.

gnocchiclient/tests/unit/test_utils.py
@@ -96,3 +96,10 @@ class SearchQueryBuilderTest(base.BaseTestCase):
             {"start": "2016-02-10T13:54:53+00:00",
              "stop": "2016-02-10T13:56:42+02:00"}),
             expected)
+
+        self.assertEqual(
+            "groupby=foo&groupby=bar",
+            utils.dict_to_querystring({
+                "groupby": ["foo", "bar"]
+            }),
+        )

commit 98bfcb757f99dbf3798fcf90358d1ce273e9adb3

utils: include 0 values
Commit 36fc0bb74267ec71afbaea1dc350b38e64497496 introduced a regression
by filtering on boolean False rather than on None when iterating over
items.

Change-Id: I66542931bd218c584e4eefe198c51e346bc09c0f

gnocchiclient/utils.py
@@ -178,7 +178,7 @@ def dict_from_parsed_args(parsed_args, attrs):
 def dict_to_querystring(objs):
     strings = []
     for k, values in objs.items():
-        if values:
+        if values is not None:
             if not isinstance(values, (list, tuple)):
                 values = [values]
             strings.append("&".join(

gnocchiclient/tests/unit/test_utils.py
@@ -103,3 +103,11 @@ class SearchQueryBuilderTest(base.BaseTestCase):
                 "groupby": ["foo", "bar"]
             }),
         )
+
+        self.assertEqual(
+            "groupby=foo&groupby=bar&overlap=0",
+            utils.dict_to_querystring({
+                "groupby": ["foo", "bar"],
+                "overlap": 0,
+            }),
+        )