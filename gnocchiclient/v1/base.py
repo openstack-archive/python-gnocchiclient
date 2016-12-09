f81a67d1c4ce808e6e72c5237b485dfc1ea35185
gengchc2 <geng.changcai2@zte.com.cn>
2016-12-09 10:02:30 +0800

    Replace six.iteritems() with .items()
    1.As mentioned in [1], we should avoid using
    six.iteritems to achieve iterators. We can
    use dict.items instead, as it will return
    iterators in PY3 as well. And dict.items/keys
    will more readable. 2.In py2, the performance
    about list should be negligible, see the link [2].
    [1] https://wiki.openstack.org/wiki/Python3
    [2] http://lists.openstack.org/pipermail/openstack-dev/2015-June/066391.html
    
    Change-Id: Ib47c9fe31ada2831c0562b9255158f74ba9ddcca

--- gnocchiclient/v1/base.py
+++ gnocchiclient/v1/base.py
@@ -13,8 +13,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import six
-
 
 class Manager(object):
     DEFAULT_HEADERS = {
@@ -26,7 +24,7 @@ class Manager(object):
 
     def _set_default_headers(self, kwargs):
         headers = kwargs.get('headers', {})
-        for k, v in six.iteritems(self.DEFAULT_HEADERS):
+        for k, v in self.DEFAULT_HEADERS.items():
             if k not in headers:
                 headers[k] = v
         kwargs['headers'] = headers
