b79d133b0702de40d4ee7ea8d8fa447fd34df91c
Julien Danjou <julien@danjou.info>
2016-12-20 16:27:50 +0100

    Stop always encoding resource
    This makes sure we do not try to guess the mechanism used by the server if it
    ever changes.
    
    The test is changed to not make UUID with / in it working. This won't
    work for a lot of reasons, and it should probably not work, so it's the
    responsibility of the caller to not use a / in the resource id.
    
    Change-Id: Ic7ce4033ee13c65e6f542f52ebf15d8f271deee6

--- gnocchiclient/tests/functional/test_resource.py
+++ gnocchiclient/tests/functional/test_resource.py
@@ -13,14 +13,11 @@
 import uuid
 
 from gnocchiclient.tests.functional import base
-from gnocchiclient import utils
 
 
 class ResourceClientTest(base.ClientTestBase):
     RESOURCE_ID = str(uuid.uuid4())
-    RESOURCE_ID2 = str(uuid.uuid4())
-    RAW_RESOURCE_ID2 = str(uuid.uuid4()) + "/foo"
-    RESOURCE_ID2 = utils.encode_resource_id(RAW_RESOURCE_ID2)
+    RESOURCE_ID2 = "foo"
     PROJECT_ID = str(uuid.uuid4())
 
     def test_help(self):
@@ -123,10 +120,9 @@ class ResourceClientTest(base.ClientTestBase):
         result = self.gnocchi(
             'resource', params=("create %s -t generic "
                                 "-a project_id:%s"
-                                ) % (self.RAW_RESOURCE_ID2, self.PROJECT_ID))
+                                ) % (self.RESOURCE_ID2, self.PROJECT_ID))
         resource2 = self.details_multiple(result)[0]
-        self.assertEqual(self.RESOURCE_ID2, resource2["id"])
-        self.assertEqual(self.RAW_RESOURCE_ID2,
+        self.assertEqual(self.RESOURCE_ID2,
                          resource2["original_resource_id"])
         self.assertEqual(self.PROJECT_ID, resource2["project_id"])
         self.assertNotEqual('None', resource2["started_at"])
@@ -141,7 +137,8 @@ class ResourceClientTest(base.ClientTestBase):
                                       "--limit 1"
                                       ) % (self.PROJECT_ID, self.RESOURCE_ID))
         resource_limit = self.parser.listing(result)[0]
-        self.assertEqual(self.RESOURCE_ID2, resource_limit["id"])
+        self.assertEqual(self.RESOURCE_ID2,
+                         resource_limit["original_resource_id"])
         self.assertEqual(self.PROJECT_ID, resource_limit["project_id"])
         self.assertEqual(resource2["started_at"], resource_limit["started_at"])
 
@@ -180,7 +177,7 @@ class ResourceClientTest(base.ClientTestBase):
         resource1 = self.details_multiple(result1)[0]
         resource2 = self.details_multiple(result2)[0]
         self.assertEqual(self.RESOURCE_ID, resource1['id'])
-        self.assertEqual(self.RESOURCE_ID2, resource2['id'])
+        self.assertEqual(self.RESOURCE_ID2, resource2['original_resource_id'])
         result3 = self.gnocchi(
             'resource batch delete ',
             params=("'id in [%s, %s]' "
